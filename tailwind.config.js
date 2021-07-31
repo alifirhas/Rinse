697f1596d9b5eccd0cbe9a96a74f4761d517f63b
irhasalif <irhasalif@gmail.com>
2021-07-31 23:35:16 +0700

    install daisyui, test template, dan css tambahan
    Menambah class virtual height
    
    note khusus tailwind di jetstream tidak lengkap. jadi jika ada class yang tidak jalan tetapi ada di dokumentasi, jalankan npm run dev agar classnya ditambahkan kembali

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,18 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            height: {
+				"10v": "10vh",
+				"20v": "20vh",
+				"30v": "30vh",
+				"40v": "40vh",
+				"50v": "50vh",
+				"60v": "60vh",
+				"70v": "70vh",
+				"80v": "80vh",
+				"90v": "90vh",
+				"100v": "100vh",
+			},
         },
     },
 
@@ -23,5 +35,9 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [
+        require('@tailwindcss/forms'), 
+        require('@tailwindcss/typography'),
+        require('daisyui'),
+    ],
 };
