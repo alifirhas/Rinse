a4f00190fc944dd339002734ee866f96683b608e
irhasalif <irhasalif@gmail.com>
2021-07-31 23:35:16 +0700

    Revert "install daisyui, test template, dan css tambahan"
    This reverts commit 697f1596d9b5eccd0cbe9a96a74f4761d517f63b.

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,3 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
-
-Route::get('test', function () {
-    return view('test');
-});
