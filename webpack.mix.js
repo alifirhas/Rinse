b2d9707faf7c887b14e84d63acecbd0ea9ce7535
irhasalif <irhasalif@gmail.com>
2021-08-01 18:33:06 +0700

    Revert "Matikan notifikasi laravel mix build successful"
    This reverts commit bbd0e56e234fa1e4a28d856d14696bb9a77a32f2.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,9 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-
-mix.disableNotifications();
-
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
